565bc1dbb4e6719499cfd9b2811ac91114445fde
Kara <palumbokara@yahoo.com>
2021-08-18 20:31:47 -0600

    updated search filter

--- src/pets/PetList.js
+++ src/pets/PetList.js
@@ -10,12 +10,28 @@ function PetList() {
   const [pets, setPets] = useState(null);
   const [selectValue, setSelectValue] = useState("");
   const [petNames, setPetNames] = useState([]);
+  const [petColors, setPetColors] = useState([]);
+  const [petGenders, setPetGenders] = useState(new Set());
+  const [petAges, setPetAges] = useState(new Set());
 
   const petInfo = async (name) => {
     const p = await PetfinderApi.getPets(name);
     const names = p.map((pet) => pet.name);
+    const colors = p.map((pet) => pet.color);
+    const genders = p.map((pet) => pet.gender);
+    const ages = p.map((pet) => pet.age);
+
     setPets(p);
     setPetNames(names);
+    setPetColors(new Set(colors));
+    setPetGenders(new Set(genders));
+    setPetAges(new Set(ages));
+
+    console.log("NEW SET", new Set(genders));
+    // console.log("NEW SET", new Set(ages));
+    // console.log("petGenders set", petGenders);
+    console.log("petGenders arr", [...petGenders]);
+    // console.log("petAges arr", [...petAges]);
   };
 
   useEffect(() => {
@@ -30,12 +46,35 @@ function PetList() {
     setPets(p);
   };
 
+  const handleColorSelect = async (event) => {
+    setSelectValue(event.target.value);
+    const isAll =
+      event.target.value === "All Colors" ? null : event.target.value;
+    const p = await PetfinderApi.getPets(isAll);
+    setPets(p);
+  };
+
+  const handleGenderSelect = async (event) => {
+    setSelectValue(event.target.value);
+    const isAll =
+      event.target.value === "All Genders" ? null : event.target.value;
+    const p = await PetfinderApi.getPets(isAll);
+    setPets(p);
+  };
+
+  const handleAgeSelect = async (event) => {
+    setSelectValue(event.target.value);
+    const isAll = event.target.value === "All Ages" ? null : event.target.value;
+    const p = await PetfinderApi.getPets(isAll);
+    setPets(p);
+  };
+
   if (!pets) return <Loading />;
 
   return (
     <div>
-      <FormGroup style={{ maxWidth: 200 }}>
-        <Label for="exampleSelect">Filter by Name</Label>
+      <FormGroup style={{ maxWidth: 300 }}>
+        <Label for="nameSelect">Filter by Name</Label>
         <Input
           onChange={handleNameSelect}
           type="select"
@@ -44,6 +83,54 @@ function PetList() {
         >
           <option>All Names</option>
           {petNames.map((pet, index) => (
+            <option value={pet} key={index}>
+              {pet}
+            </option>
+          ))}
+        </Input>
+
+        <Label for="colorSelect">Filter by Color</Label>
+        <Input
+          onChange={handleColorSelect}
+          type="select"
+          name="select"
+          value={selectValue}
+        >
+          <option>All Colors</option>
+
+          {[...petColors].map((pet, index) => (
+            <option value={pet} key={index}>
+              {pet}
+            </option>
+          ))}
+        </Input>
+
+        <Label for="genderSelect">Filter by Gender</Label>
+        <Input
+          onChange={handleGenderSelect}
+          type="select"
+          name="select"
+          value={selectValue}
+        >
+          <option>All Genders</option>
+
+          {[...petGenders].map((pet, index) => (
+            <option value={pet} key={index}>
+              {pet}
+            </option>
+          ))}
+        </Input>
+
+        <Label for="genderSelect">Filter by Age</Label>
+        <Input
+          onChange={handleAgeSelect}
+          type="select"
+          name="select"
+          value={selectValue}
+        >
+          <option>All Ages</option>
+
+          {[...petAges].map((pet, index) => (
             <option value={pet} key={index}>
               {pet}
             </option>
